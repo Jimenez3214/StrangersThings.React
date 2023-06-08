4f8b9ff2ce100556c7c85a9e42a10e3573373d6b
Vimaser <thfeske@gmail.com>
2023-06-07 22:01:30 -0500

    fixed issue

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
 
 // ReactDOM.render(<App/>, document.getElementById("app"));
 
-import React from 'react';
+/* import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './app.js';
@@ -14,4 +14,11 @@ import App from './app.js';
 ReactDOM.render(
     <App />,
     document.getElementById('app'),
-)
+) */
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./app.js";
+
+ReactDOM.render(<App />, document.getElementById("app"));
