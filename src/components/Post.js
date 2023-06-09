621bfaaa93678671ba3cc2309be28c37377a7dcb
Vimaser <thfeske@gmail.com>
2023-06-09 17:31:55 -0500

    JSX

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { BASE_URL } from '../api';
 
+
 const Post = ({ postId }) => {
     const [post, setPost] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
@@ -8,10 +9,10 @@ const Post = ({ postId }) => {
     useEffect(() => {
         const fetchPost = async() => {
             try {
-                const response = await fetch(`${BASE_URL}/posts`);
+                const response = await fetch(`${BASE_URL}/posts/${postId}`);
                 const data = await response.json();
-                // console.log(data)
-                setPost(data);
+                //console.log(data)
+                setPost(data.post);
                 setIsLoading(false);
             } catch (error) {
                 console.log('Error fetch', error);
@@ -29,6 +30,8 @@ const Post = ({ postId }) => {
                 <div>
                     <p>Title: {post.title}</p>
                     <p>Description: {post.description}</p>
+                    <p>Price: {post.price}</p>
+                    <p>Will Deliver: {post.willDeliver ? 'Yes' : 'No'}</p>
                     <button>Edit Post</button>
                     <button>Delete Post</button>
                 </div>
