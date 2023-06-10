d87f5be584fec552e5fe9ad50a3acd2d8770d0ff
Vimaser <thfeske@gmail.com>
2023-06-09 19:13:46 -0500

    need token

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,17 @@
-const COHORT_NAME = '2303-ftb-et-web-pt';
+export const COHORT_NAME = '2303-ftb-et-web-pt';
 
 export const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
 
+export const TOKEN = 'Get our Auth Token Dude!';
+
 const fetchData = async () => {
   try {
-    const response = await fetch(`${BASE_URL}/posts`);
+    const response = await fetch(`${BASE_URL}/posts`, {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ${TOKEN}',
+        },
+    });
     const data = await response.json();
     console.log('Returned data:', data);
     return data;
