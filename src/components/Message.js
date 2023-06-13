cabe23f69f3a6a91f164e7394499fe345e2d3e5c
Jimenez3214 <jimenezeduardo83@gmail.com>
2023-06-12 20:31:17 -0500

    Search and Messeges

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,12 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { BASE_URL } from '../api';
 
 const Message = () => {
+    const [message, setMessages] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(()=> {
+        const fetchMessages = async () => {
+            try{
+                const response = await fetch (`${BASE_URL}/post/POST_ID/messages`)
+                const data = await response.json();
+
+                setMessages(data);
+                setIsLoading(false);
+            }catch(error){
+                console.log('Error is a fetching!', error);
+
+            }
+        }
+        fetchMessages();
+    })
+
+    const handleSendMessage = (userId, message) => {
+        console.log(`sending message to user ${userId}: ${message}`)
+    };
+
     return (
-        <div id='Message'>
-            <h1>Message</h1>
-        </div>
+        <div>
+        <h1>Messages</h1>
+        {isLoading ?(
+            <p>Loading messages...</p>
+        ) : (
+            <div>
+            {message.length === 0 ? (
+                <p>No messages available</p>
+            ) : (
+                <ul>
+                    {message.map((message) => (
+                        <li key={message.id}>
+                            <p>From: {message.sender} </p>
+                            <p>To: {message.recipient}</p>
+                            <p>{message.content}</p>
+                        </li>
+                    ))}
+                </ul>
+            )}
+            </div>
+        )}
+    </div>
+    {/* form for sending message */}
+    <form
+        onSubmit={(e) => {
+            e.preventDefult();
+            const userId = ''; // Provide userid
+            const message = ''; // Get message
+            handleSendMessage(userId, message);
+        }}
+    >
+        <input type="text" placeholder="Recipient ID" />
+        <input type="text" placeholder="Message" />
+        <button type="submit">Send Message</button>
+    </form>
+</div>
       
     );
 };
+
+
 
 export default Message;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { BASE_URL } from '../api';
 
 const Search = () => {
     const [searchQuery, setSearchQuery] = useState('');
@@ -11,10 +12,10 @@ const Search = () => {
         setIsLoading(true);
 
         try {
-            const response = await fetch();
+            const response = await fetch(`${BASE_URL}/posts`);
             const data = await response.json();
-
-            setSearchResults(data);
+            console.log(response);
+            setSearchResults(data.post);
         } catch (error) {
             console.log('Error during the search', error);
         }
@@ -22,36 +23,37 @@ const Search = () => {
     };
 
     return (
-        <div id='Search'>
-            <h1>Search</h1>
-            <form onSubmit={handleSearch}>
-                <input
-                type='text'
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target)}                
-                placeholder='search query'
-                required
-                />
-                <button type='submit'>Search</button>
-            </form>
-            {isLoading ? (
-                <p>Loading...</p>
-            ) : (
-                <div>
-                    {searchResults.length > 0 ?(
-                        <ul>
-                            {searchResults.map((results) => (
-                                  <li key={result.id}>{result.title}</li>
-                            ))}
-                        </ul>
-                    ) : (
-                        <p>No results found</p>
-                    )}    
-                </div>
-            )}
-        </div>
+        <div id="Search">
+          <h1>Search</h1>
+          <form onSubmit={handleSearch}>
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              placeholder="search query"
+              
+            />
+            <button type="submit">Search</button>
+          </form>
       
-    );
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <div>
+              {searchResults && searchResults.length > 0 ? (
+                <ul>
+                  {searchResults.map((result) => (
+                    <li key={result.id}>{result.title}</li>
+                  ))}
+                </ul>
+              ) : (
+                <p>No results found</p>
+              )}
+            </div>
+          )}
+        </div>
+      );
 };
+
 
 export default Search;
