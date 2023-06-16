0eeee6b87a8df46643893bf36538cffce895c8c1
Vimaser <thfeske@gmail.com>
2023-06-16 15:23:42 -0500

    small changes

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BASE_URL } from '../api';
-//import { AuthContext } from '../AuthContext';
+import { AuthContext } from '../app';
+
 
 
 
